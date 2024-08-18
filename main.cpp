774e0bffa19855908fab1d774d4021185c1c8701
Xinyang Wang <136667147+mrkingseasheep@users.noreply.github.com>
2024-08-18 18:18:44 -0400

    Changing to reverse polish

--- main.cpp
+++ main.cpp
@@ -1,72 +1,20 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <regex>
 #include <string>
 #include <vector>
 
-union Value {
-    double num;
-    char op;
-};
-
-class Node {
-  public:
-    Value value;
-    Node* lVal;
-    Node* rVal;
-    bool isNum = true;
-
-    Node() {};
-
-    void setOp(char op) {
-        value.op = op;
-        isNum = true;
-    }
-
-    void setNum(double num) {
-        value.num = num;
-        isNum = false;
-    }
-};
-
-double parseNum(const char* eq) {}
-
-Node* parseEq(char* eqBeg, char* eqEnd) {
-    std::vector<char*> bracket;
-    for (char* curNode = eqBeg; curNode != eqEnd; ++curNode) {
-        if (*curNode == '(') {
-            bracket.push_back(curNode);
-        }
-    }
-}
-
-double solve(std::string& eq) {
-    std::cout << "solving..." << std::endl;
-    std::cout << eq << std::endl;
-    return 0;
-}
-
-// basic implementation
-double simple_solve(std::string& eq) {
-    eq.erase(std::remove_if(eq.begin(), eq.end(), isspace), eq.end());
-    int lNum = eq.at(0) - 48;
-    char symbol = eq.at(1);
-    int rNum = eq.at(2) - 48;
-
-    switch (symbol) {
-    case '+':
-        return lNum + rNum;
-    case '-':
-        return lNum - rNum;
-    case '*':
-        return lNum * rNum;
-    case '/':
-        return (double)lNum / (double)rNum;
-    case '^':
-        return pow(lNum, rNum);
-    default:
-        std::cout << "Enter a valid symbol: " << std::endl;
-        return -1;
+// no more tree solution
+// using shunting yard algo
+// also reverse polish notation
+// https://stackoverflow.com/questions/11627440/regex-c-extract-substring
+void parseToReversePolish(const char* eq, std::vector<std::string>& rPolishEq) {
+    std::regex numRgx("\\d*.?\\d*");
+    std::smatch match;
+    /*const std::string temp = "042.6240";*/
+    if (std::regex_search(eq, eq + 1, match, numRgx)) {
+        std::cout << match[0] << std::endl;
     }
 }
 
@@ -79,9 +27,11 @@ int main() {
             continue;
         }
 
+        std::vector<std::string> rPolishEq;
         eq.erase(std::remove_if(eq.begin(), eq.end(), isspace), eq.end());
-        double ans = simple_solve(eq);
-        std::cout << ans << std::endl;
+        parseToReversePolish(eq.c_str(), rPolishEq);
+        /*double ans = simple_solve(eq);*/
+        /*std::cout << ans << std::endl;*/
         std::cout << "Enter in your equation: " << std::endl;
     }
 
