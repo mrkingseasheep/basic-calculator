54e769bd89ec5811b8ae59a0c1a47867661e98a8
Xinyang Wang <136667147+mrkingseasheep@users.noreply.github.com>
2024-08-18 20:12:41 -0400

    Move nums into rev polish arr

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <algorithm>
+#include <cctype>
 #include <cmath>
 #include <iostream>
 #include <queue>
 #include <regex>
+#include <stack>
 #include <string>
 #include <vector>
 
@@ -10,21 +12,57 @@
 // using shunting yard algo
 // also reverse polish notation
 // https://stackoverflow.com/questions/11627440/regex-c-extract-substring
-void parseToReversePolish(std::string& eq,
-                          std::vector<std::string>& rPolishEq) {
+void tokenizeEq(std::string& eq, std::queue<std::string>& actionQueue) {
     // TODO all chars must be lowercase
+    // https://stackoverflow.com/questions/21667295/how-to-match-multiple-results-using-stdregex
     const std::regex findVal("[^\\d\\.a-z]|[a-z]{3}|\\d*\\.?\\d+");
     std::regex_token_iterator<std::string::iterator> rend;
     std::regex_token_iterator<std::string::iterator> value(eq.begin(), eq.end(),
                                                            findVal);
-    std::queue<std::string> eqQueue;
+
     while (value != rend) {
-        std::cout << *value++ << std::endl;
-        /*eqQueue.push(*value++);*/
-        /*std::cout << eqQueue.front() << std::endl;*/
-        /*eqQueue.pop();*/
+        actionQueue.push(*value++);
     }
 }
+
+// Which operators have the greatest priority
+// ()
+// ^
+// */
+// +-
+// > < >= <= =
+std::vector<std::string>
+parseToReversePolish(std::string& eq, std::queue<std::string>& actionQueue) {
+    std::stack<std::string> eqOperator;
+    std::vector<std::string> action;
+
+    while (!actionQueue.empty()) {
+        std::string curAction = actionQueue.front();
+        actionQueue.pop();
+
+        if (curAction.at(0) == '.' || isdigit(curAction.at(0))) {
+            // checks if is digit or not
+            // remove for final
+            double num;
+            try {
+                num = std::stod(curAction);
+            } catch (...) {
+                std::cerr << "Cannot change string to num: " << curAction
+                          << std::endl;
+                break;
+            }
+            action.push_back(curAction);
+            continue;
+        }
+
+        // CHANGE ME AFTER DEBUG
+        action.push_back("+");
+    }
+
+    return action;
+}
+
+double solveReversePolishEq(std::vector<std::string>& action) {}
 
 int main() {
     std::string eq;
@@ -35,11 +73,15 @@ int main() {
             continue;
         }
 
-        std::vector<std::string> rPolishEq;
+        std::queue<std::string> actionQueue;
         eq.erase(std::remove_if(eq.begin(), eq.end(), isspace), eq.end());
-        parseToReversePolish(eq, rPolishEq);
-        /*double ans = simple_solve(eq);*/
-        /*std::cout << ans << std::endl;*/
+        tokenizeEq(eq, actionQueue);
+        std::vector<std::string> action = parseToReversePolish(eq, actionQueue);
+
+        for (auto temp : action) {
+            std::cout << temp << std::endl;
+        }
+
         std::cout << "Enter in your equation: " << std::endl;
     }
 
